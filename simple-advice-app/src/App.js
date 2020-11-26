d9feaed2bf745cb7cd0f0e6ca9f4300088c95622
Rene Promesse Ntirenganya <69194903+renepromesse@users.noreply.github.com>
2020-11-26 14:32:47 +0200

    creates a simple page that returns an advice message

---
+++ simple-advice-app/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+    state = { 
+        advice : ''
+    }
+
+    //method that road first when the page starts in react
+    componentDidMount = () =>{
+        console.log('COMPONENT DID MOUNT');
+        this.getAdvice();
+    }
+    
+    render() { 
+        const {advice} = this.state;
+        return ( 
+            <div className="appMain">
+                <div className="whiteCard">
+                    <h4 className="cardHeading">"{advice}"</h4>
+                    <button onClick={this.getAdvice} className="adviceButton">Get Me Advice</button>
+                </div>
+                
+            </div>
+        
+        );
+    }
+
+   
+
+    getAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+        .then(( result) => { 
+            const {advice}= result.data.slip;
+            console.log(advice);
+            this.setState({advice});
+        })
+        .catch((error) => {console.log(error)});
+    }
+
+}
+ 
+export default App;
